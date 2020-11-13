ff0466e954c7557de94bbd7d7995422ca7063919
jchxch <70745797+jchxch@users.noreply.github.com>
2020-11-13 23:42:42 +0800

    java与python数据交流初步尝试
    python for service

--- jch/test3.py
+++ jch/test3.py
@@ -1,54 +1,71 @@
-from bs4 import BeautifulSoup
-from bs4 import UnicodeDammit
-import urllib.request
-import urllib.parse
-import time
-#start_time=time.clock()
-start_url="http://www.weather.com.cn/weather/101280601.shtml"
-headers={
-    "User-Agent":"Mozilla/5.0(Windows NT 6.0 x64;en-US;rv:1.9pre)Gecko/2"
-}
-def imageSpider(start_url):
-    try:
-        urls=[]
-        req=urllib.request.Request(start_url,headers=headers)
-        data=urllib.request.urlopen(req)
-        data =data.read()
-        dammit=UnicodeDammit(data,["utf-8","gbk"])
-        data=dammit.unicode_markup
-        soup=BeautifulSoup(data,"lxml")
-        images=soup.select("img")
-        for image in images:
-            try:
-                src=image["src"]
-                url=urllib.parse.urljoin(start_url,src)
-                if url not in urls:
-                    urls.append(url)
-                    print(url)
-                    download(url)
-            except Exception as err:
-                print(err)
-    except Exception as err:
-        print(err)
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import url_for
+import MySQLdb
+import re
+import sys
+import types
+app=Flask(__name__)
 
-def download(url):
-    global count
-    try:
-        count=count+1
-        if(url[len(url)-4]=='.'):
-            ext=url[len(url)-4:]
-        else:
-            ext=''
-        req=urllib.request.Request(url,headers=headers)
-        data=urllib.request.urlopen(req,timeout=100)
-        data=data.read()
-        fobj=open("images\\"+str(count)+ext,"wb")
-        fobj.write(data)
-        fobj.close()
-        print("download"+str(count)+ext)
-    except Exception as err:
-        print(err)
-count=0
-imageSpider(start_url)
-#end_time=time.clock()
-#print("耗时：",end_time-start_time)
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+conn=MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user='root',
+    passwd='a821200725',
+    db='webjob',
+    charset='utf8'
+)
+
+#sys.setdefaultencoding('utf-8')
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        InNumber=request.form['InNumber']
+        InNumber=numsort(InNumber)
+        return render_template('index.html',result=InNumber)
+    else: 
+        return render_template('index.html')
+
+@app.route('/province',methods=['POST','GET'])
+def province():
+    if request.method=='POST':
+        rev=request.get_json()['city']
+        result=selcity(rev)
+        return result
+    else:
+        return render_template('province.html')
+
+
+def numsort(number):
+    print number
+#    tmp=number.split(' *')
+    tmp=re.split("\s+",number)
+    print tmp
+    for i in range(len(tmp)):
+        tmp[i]=int(tmp[i])
+    print tmp
+    tmp.sort()
+    res=""
+    for i in tmp:
+        res+=(str(i)+" ")
+    return res
+
+def selcity(city):
+    sql="select litcity from Bigcity where city='"+city+"'"
+    cur=conn.cursor()
+    cur.execute(sql)
+    result=cur.fetchone()
+    results=result[0]
+#    results=results.decode("unicode-escape")
+    return results
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
